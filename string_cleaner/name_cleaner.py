477b7afe9e583fbecca5945bcb9e1dd444ab6a3e
Zazralt Magic <zazralt@gmail.com>
2025-06-22 12:52:24 +0200

    added normalize_notation

--- string_cleaner/name_cleaner.py
+++ string_cleaner/name_cleaner.py
@@ -43,3 +43,63 @@ def detect_naming_convention(text: str) -> str:
     if re.fullmatch(r'[a-z]+', text):
         return "lowercase"
     return "unknown"
+
+def normalize_notation(name: str, notation: str = 'default', case: str = 'default', delimiter: str = ' ', preserve_case: bool = False) -> str:
+    """
+    Convert a string into a specified notation format.
+
+    Parameters:
+        name (str): Input string to format.
+        notation (str): 'default', 'snake', 'camel', 'pascal', or 'title'.
+        case (str): 'default', 'lower', 'upper', 'title', 'capitalize'.
+        delimiter (str): Delimiter to use (e.g., ' ', '_', '').
+        preserve_case (bool): If True, do not modify letter case.
+
+    Returns:
+        str: The formatted string.
+    """
+
+    # clean notation
+    name = re.sub('([a-z0-9])([A-Z])', r'\1 \2', name)          # space for camel cases
+    name = re.sub('([A-Z])([A-Z])([a-z]+)', r'\1 \2\3', name)   # space after acronyms
+    name = re.sub('([0-9])([a-z])', r'\1 \2', name)             # space after numbers
+    name = re.sub('([A-z])([0-9])', r'\1 \2', name)             # space before numbers
+    name = ' '.join(word for word in name.split('_'))           # clean snake cases
+    name = re.sub('[^A-z0-9]', ' ', name).strip()               # replace special characters with delimiter
+    name = re.sub('\s+', ' ', name).strip()                     # remove multiple spaces
+
+    # prepare notation
+    if notation == 'snake':
+        case = 'lower'
+        delimiter = '_'
+    elif notation in ['pascal', 'camel']:
+        case = 'title'
+        delimiter = ''
+    elif notation == 'title':
+        case = 'title'
+        delimiter = ' '
+
+    # prepare case
+    if not preserve_case:
+        if case == 'default':
+            if name.isupper():
+                name = name.lower()
+            else:
+                # convert title to lower and leave acronyms upper
+                name = ' '.join([word.lower() if word.istitle() else word for word in name.split()])
+        if case == 'lower':
+            name = name.lower()
+        if case == 'upper':
+            name = name.upper()
+        if case == 'title':
+            name = name.title()
+        if case == 'capitalize':
+            name = name.capitalize()
+
+    # prepare delimiter
+    if delimiter != ' ':
+        name = re.sub(' ', delimiter, name)
+    if notation == 'camel':
+         name = name[:1].lower() + name[1:]
+
+    return name
