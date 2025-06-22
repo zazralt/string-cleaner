c17ffcf1865fa2e32c067bd0a0938c4d36456b68
Zazralt Magic <zazralt@gmail.com>
2025-06-22 14:09:42 +0200

    Update __init__.py

--- string_cleaner/__init__.py
+++ string_cleaner/__init__.py
@@ -1,4 +1,1 @@
-#from .name_cleaner import camel_to_snake, detect_naming_convention, normalize_notation
-#__all__ = ["camel_to_snake", "detect_naming_convention", "normalize_notation"]
-
-from string_cleaner import *
+from .name_cleaner import *
